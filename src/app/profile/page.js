40360b946bc51fc2235bccb6e885baea3c0567ad
unknown <phivanduc325@gmail.com>
2023-12-10 22:45:05 +0700

    fix2

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,6 @@
-import ProviderLogged from "./providerLoggedComponent/ProviderLogged"
-
 export default async function ProfilePage() {
     return (
         <div className="profile-page">
-            <ProviderLogged />
         </div>
     )
 }
